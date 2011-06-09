2868932204bed98534e14573e280026dcb8d6c4c
Jonathan Goldman <me@jonnii.com>
2011-06-08 23:19:41 -0400

    Register the interceptor on ActionMailer::Base

--- lib/premailer-rails3.rb
+++ lib/premailer-rails3.rb
@@ -3,4 +3,4 @@ require 'hpricot'
 require 'premailer-rails3/premailer'
 require 'premailer-rails3/hook'
 
-Mail.register_interceptor(PremailerRails::Hook)
+ActionMailer::Base.register_interceptor(PremailerRails::Hook)
