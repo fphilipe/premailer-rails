6c4830d344b73ec30742816b8ad57b091e6a42eb
Rolly Fordham <rolly@luma.co.nz>
2011-06-28 17:29:10 +1200

    Scan the html body for css files and include them

--- lib/premailer-rails3/hook.rb
+++ lib/premailer-rails3/hook.rb
@@ -1,7 +1,14 @@
 module PremailerRails
   class Hook
     def self.delivering_email(message)
-      premailer = Premailer.new(message.body.to_s)
+      options = {}
+
+      # Grab the html part, if there is one, and grab all css file references
+      unless (html_part = message.html_part).blank?
+        options[:css] = (Hpricot[html_part]/'link[@type="text/css"]').colect { |css_file| css_file.attributes['href'] }
+      end
+
+      premailer = Premailer.new(message.body.to_s, options)
 
       # reset the body and add two new bodies with appropriate types
       message.body = nil
