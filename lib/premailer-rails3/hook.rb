284bc5eb31eaae3e001d2904ee7309dcb7b6d825
Philipe Fatio <philipe.fatio@gmail.com>
2011-08-07 16:05:55 +0200

    Improve code, add caching for default css with support for assets pipeline.

--- lib/premailer-rails3/hook.rb
+++ lib/premailer-rails3/hook.rb
@@ -1,40 +1,24 @@
 module PremailerRails
   class Hook
     def self.delivering_email(message)
-      # If there's no HTML part then there isn't much we can do anyway. 
-      return if message.html_part == nil
+      if html_part = message.html_part or (
+        message.content_type =~ /text\/html/ and
+        html_part = message.body
+      )
+        premailer = Premailer.new(html_part.to_s)
 
-      html_part = message.html_part
-      premailer = Premailer.new(html_part.body.to_s, { :css => extract_css_paths_from_body(html_part.body) })
+        # reset the body and add two new bodies with appropriate types
+        message.body = nil
 
-      # reset the body and add two new bodies with appropriate types
-      message.body = nil
+        message.html_part do
+          content_type "text/html; charset=utf-8"
+          body premailer.to_inline_css
+        end
 
-      message.html_part do
-        content_type "text/html; charset=utf-8"
-        body premailer.to_inline_css
-      end
-
-      message.text_part do
-        body premailer.to_plain_text
-      end
-    end
-
-    protected
-
-    # Scan the HTML mailer template for CSS files, specifically Link tags with types 
-    # of text/css  (other ways of including CSS are not supported). Once it's finds
-    # them it returns an Array that contains the URL of each of the CSS files.
-    def self.extract_css_paths_from_body(html_body)
-      css_file_paths = Hpricot(html_body.to_s).search('link[@type="text/css"]').collect do |css_file| 
-        if css_file.attributes['href'].include?('?')
-          css_file.attributes['href'][0..(css_file.attributes['href'].index('?') - 1)]
-        else
-          css_file.attributes['href']
+        message.text_part do
+          body premailer.to_plain_text
         end
       end
-
-      css_file_paths.empty?? nil : css_file_paths.to_a
     end
   end
 end
--- lib/premailer-rails3/premailer.rb
+++ lib/premailer-rails3/premailer.rb
@@ -1,30 +1,63 @@
 module PremailerRails
   class Premailer < ::Premailer
-    def initialize(html, options = {})
-      super(html, :with_html_string => true,
-                  :adapter => :hpricot,
-                  :css => css_file_for_options(options))
+    @@_css_cache = {}
+
+    def initialize(html)
+      load_html(html)
+      options = {
+        :with_html_string => true,
+        :adapter          => :hpricot
+      }.merge css_options
+      super(html, options)
     end
 
+    # HTML can be accessed by attr_accessor doc
     def load_html(string)
-      Hpricot(string)
+      @doc ||= Hpricot(string)
     end
 
     protected
 
-    def css_file_for_options(options)
-      if options.include?(:css)
-        options[:css].is_a?(Array) ? options[:css] : [options[:css]]
-      else
-        default_css_file
+    def css_options
+      options = {
+        :css => linked_css_files
+      }
+
+      if linked_css_files.empty?
+        options[:css_string] = default_css_file
       end
+
+      options
     end
-    
+
     def default_css_file
-      if defined? Hassle and Rails.configuration.middleware.include? Hassle
-        ['tmp/hassle/stylesheets/email.css']
-      else
-        ['public/stylesheets/email.css']
+      # Don't cache in development.
+      if Rails.env.development? or not @@_css_cache.include? :default
+        @@_css_cache[:default] =
+          if defined? Hassle and Rails.configuration.middleware.include? Hassle
+            File.read("#{Rails.root}/tmp/hassle/stylesheets/email.css")
+          elsif Rails.configuration.assets.try(:enabled)
+            Rails.application.assets.find_asset('email.css').body
+          else
+            File.read("#{Rails.root}/public/stylesheets/email.css")
+          end
+      end
+      @@_css_cache[:default]
+    rescue => ex
+      puts ex.message
+      @@_css_cache[:default] = nil
+    end
+
+    # Scan the HTML mailer template for CSS files, specifically link tags with
+    # types of text/css (other ways of including CSS are not supported).
+    def linked_css_files
+      @_linked_css_files ||= doc.search('link[@type="text/css"]').collect do |l|
+        href = l.attributes['href']
+        if href.include? '?'
+          href[0..(href.index('?') - 1)]
+        else
+          href
+        end
       end
     end
   end
