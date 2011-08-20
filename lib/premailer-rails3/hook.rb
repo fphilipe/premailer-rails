0a33bfa1543ea0c3337b26649f566ca9be6b8c70
Florian Dütsch <mail@florian-duetsch.de>
2011-08-20 04:54:35 +0300

    Do not override already rendered plain-text mail

--- lib/premailer-rails3/hook.rb
+++ lib/premailer-rails3/hook.rb
@@ -6,6 +6,7 @@ module PremailerRails
         html_part = message.body
       )
         premailer = Premailer.new(html_part.body.to_s)
+        plain_text = message.text_part.try(:body).try(:to_s)
 
         # reset the body and add two new bodies with appropriate types
         message.body = nil
@@ -15,8 +16,9 @@ module PremailerRails
           body premailer.to_inline_css
         end
 
+        plain_text = premailer.to_plain_text if plain_text.blank?
         message.text_part do
-          body premailer.to_plain_text
+          body plain_text
         end
       end
     end
