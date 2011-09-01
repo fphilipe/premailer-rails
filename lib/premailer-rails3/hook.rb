baccb04fe8cd330f8a4081e476a19059c5bdfde2
Sven Winkler <sven.winkler@nix-wie-weg.de>
2011-09-01 15:51:03 +0200

    Fix encoding-issues in Rails 3.1

--- lib/premailer-rails3/hook.rb
+++ lib/premailer-rails3/hook.rb
@@ -11,13 +11,16 @@ module PremailerRails
         # reset the body and add two new bodies with appropriate types
         message.body = nil
 
+        t_charset = message.charset
+
         message.html_part do
-          content_type "text/html; charset=utf-8"
+          content_type "text/html; charset=#{t_charset}"
           body premailer.to_inline_css
         end
 
         plain_text = premailer.to_plain_text if plain_text.blank?
         message.text_part do
+          content_type "text/plain; charset=#{t_charset}"
           body plain_text
         end
       end
