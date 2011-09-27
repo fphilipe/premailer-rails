0ca2d69a934bedae6a4f2ce03c5058da77af2fff
Leonid Shevtsov <leonid@shevtsov.me>
2011-09-27 12:36:44 -0400

    more refactoring for default css

--- lib/premailer-rails3/premailer.rb
+++ lib/premailer-rails3/premailer.rb
@@ -17,26 +17,31 @@ module PremailerRails
     def load_css_from_default_file!
       # TODO and what if there are no rules and it's normal?
       if @css_parser.to_s == ''
-        load_css_from_string(default_css_file || '')
+        load_css_from_string(default_css_file)
       end
     end
 
     def default_css_file
       # Don't cache in development.
-      if Rails.env.development? or not @@_css_cache.include? :default
+      if Rails.env.development? || !@@_css_cache.include?(:default)
         @@_css_cache[:default] =
-          if defined? Hassle and Rails.configuration.middleware.include? Hassle
-            File.read("#{Rails.root}/tmp/hassle/stylesheets/email.css")
+          if defined?(Hassle) && Rails.configuration.middleware.include?(Hassle) && File.exists?(default_css_filename_for_hassle)
+            File.read(default_css_filename_for_hassle)
           elsif Rails.configuration.try(:assets).try(:enabled)
-            Rails.application.assets.find_asset('email.css').body
-          else
-            File.read("#{Rails.root}/public/stylesheets/email.css")
+            Rails.application.assets.find_asset('email.css').try(:body) || ''
+          elsif File.exists?(default_css_filename)
+            File.read(default_css_filename)
           end
       end
       @@_css_cache[:default]
-    rescue => ex
-      puts ex.message
-      @@_css_cache[:default] = nil
+    end
+
+    def default_css_filename_for_hassle
+      File.join(Rails.root.to_s, 'tmp', 'hassle', 'stylesheets', 'email.css')
+    end
+
+    def default_css_filename
+      File.join(Rails.root.to_s, 'public', 'stylesheets', 'email.css')
     end
   end
 end
