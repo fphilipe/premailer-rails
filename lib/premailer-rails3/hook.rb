7795d35e936e06805eaac993ea93d5dcc39c45d3
welldan97 <welldan97@gmail.com>
2012-03-14 11:47:52 +0400

    Patch for emails with attachments

--- lib/premailer-rails3/hook.rb
+++ lib/premailer-rails3/hook.rb
@@ -19,15 +19,42 @@ module PremailerRails
         # IMPORTANT: Plain text part must be generated before CSS is inlined.
         # Not doing so results in CSS declarations visible in the plain text
         # part.
-        message.text_part do
-          content_type "text/plain; charset=#{charset}"
-          body premailer.to_plain_text
-        end unless message.text_part
 
-        message.html_part do
-          content_type "text/html; charset=#{charset}"
-          body premailer.to_inline_css
+        unless message.text_part
+          text_part = {
+            :content_type => "text/plain; charset=#{charset}",
+            :body => premailer.to_plain_text
+          }
         end
+
+        html_part = {
+          :content_type => "text/html; charset=#{charset}",
+          :body => premailer.to_inline_css
+        }
+
+        if message.text_part && message.html_part
+          message.html_part.body = html_part[:body]
+          message.html_part.content_type = html_part[:content_type]
+
+        elsif message.html_part
+          message.parts.delete_if { |p| p.mime_type == 'text/html' && !p.attachment? }
+
+          message.part :content_type => "multipart/alternative", :content_disposition => "inline" do |p|
+            p.part text_part
+            p.part html_part
+          end
+        else
+          message.text_part do
+            body text_part[:body]
+            content_type text_part[:content_type]
+          end
+
+          message.html_part do
+            body html_part[:body]
+            content_type html_part[:content_type]
+          end
+        end
+
       end
     end
   end
