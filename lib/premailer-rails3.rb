e429610a23673376bb4b8c7351f6f0070f962b1d
Sijawusz Pur Rahnama <sija@positionly.com>
2012-03-16 14:40:40 +0100

    Force premailer to give us UTF-8 support by default

--- lib/premailer-rails3.rb
+++ lib/premailer-rails3.rb
@@ -4,6 +4,8 @@ require 'premailer-rails3/css_helper'
 require 'premailer-rails3/premailer'
 require 'premailer-rails3/hook'
 
+require 'overrides/premailer/adapter/nokogiri'
+
 module PremailerRails
   @config = {
     :input_encoding     => 'UTF-8',
