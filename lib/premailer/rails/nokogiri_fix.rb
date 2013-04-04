2404b5b0864c61530dc496180d08dc0a3c5acf4b
Philipe Fatio <me@phili.pe>
2013-04-04 09:49:40 +0200

    Don't cover ruby 1.8 conditionals

--- lib/premailer/rails/nokogiri_fix.rb
+++ lib/premailer/rails/nokogiri_fix.rb
@@ -7,7 +7,9 @@ Premailer::Adapter::Nokogiri.module_eval do
       html = html.force_encoding('UTF-8').encode!
       ::Nokogiri::HTML(html) {|c| c.recover }
     else
+      # :nocov:
       ::Nokogiri::HTML(html, nil, 'UTF-8') {|c| c.recover }
+      # :nocov:
     end
   end
 end
