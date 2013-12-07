ad43edeb29e6489dc12baa637de88ee3a232263d
Philipe Fatio <me@phili.pe>
2013-12-07 13:32:43 +0100

    Remove ruby 1.8 code

--- lib/premailer/rails/nokogiri_fix.rb
+++ lib/premailer/rails/nokogiri_fix.rb
@@ -3,13 +3,7 @@ require 'premailer/adapter/nokogiri'
 Premailer::Adapter::Nokogiri.module_eval do
   # Patch load_html method to fix character encoding issues.
   def load_html(html)
-    if RUBY_VERSION.to_f >= 1.9
-      html = html.force_encoding('UTF-8').encode!
-      ::Nokogiri::HTML(html) {|c| c.recover }
-    else
-      # :nocov:
-      ::Nokogiri::HTML(html, nil, 'UTF-8') {|c| c.recover }
-      # :nocov:
-    end
+    html = html.force_encoding('UTF-8').encode!
+    ::Nokogiri::HTML(html) { |c| c.recover }
   end
 end
