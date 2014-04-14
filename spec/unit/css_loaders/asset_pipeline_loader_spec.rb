de8f8cbafbe913cd7afd9171a114a6bf81678e70
Mark Nadig <mark@nadigs.net>
2014-04-14 15:22:07 -0600

    asset_pipline_loader check for assets.nil? for case where assets defined, but nil (enabled=false).  updated test.

--- spec/unit/css_loaders/asset_pipeline_loader_spec.rb
+++ spec/unit/css_loaders/asset_pipeline_loader_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe Premailer::Rails::CSSLoaders::AssetPipelineLoader do
-  before do
-    assets = double(prefix: '/assets')
-    config = double(assets: assets)
-    allow(Rails).to receive(:configuration).and_return(config)
-  end
 
   describe ".file_name" do
+    before do
+      assets = double(prefix: '/assets')
+      config = double(assets: assets)
+      allow(Rails).to receive(:configuration).and_return(config)
+    end
+
     subject do
       Premailer::Rails::CSSLoaders::AssetPipelineLoader.file_name(asset)
     end
@@ -26,5 +27,22 @@ describe Premailer::Rails::CSSLoaders::AssetPipelineLoader do
       let(:asset) { 'test/20130708152545-foo-bar.css' }
       it { should == "test/20130708152545-foo-bar.css" }
     end
+
+  end
+
+  describe "no assets" do
+    before do
+      application = double(assets: nil)
+      allow(Rails).to receive(:application).and_return(application)
+    end
+
+    subject do
+      Premailer::Rails::CSSLoaders::AssetPipelineLoader.asset_pipeline_present?
+    end
+
+    context "when assets are not enabled" do
+      it { should == false }
+    end
+
   end
 end
