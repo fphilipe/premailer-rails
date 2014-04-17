13a29c27365e4d62c71014efc61120489a57b5eb
Michael Reinsch <michael@mobalean.com>
2014-04-17 16:33:20 +0900

    remove any style links we are processing, otherwise premailer will load them once more...

--- lib/premailer/rails/customized_premailer.rb
+++ lib/premailer/rails/customized_premailer.rb
@@ -11,9 +11,9 @@ class Premailer
         @options = Rails.config.merge(with_html_string: true)
         Premailer.send(:include, Adapter.find(Adapter.use))
         doc = load_html(html)
-
         options = @options.merge(css_string: CSSHelper.css_for_doc(doc))
-        super(html, options)
+
+        super(doc.to_s, options)
       end
     end
   end
