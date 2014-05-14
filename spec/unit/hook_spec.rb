c0b771f8d66194720574d6eca1bdac0c31eb8d8e
Yi Wen <ywen.github@gmail.com>
2014-05-14 16:44:36 -0500

    Complete tests

--- spec/unit/hook_spec.rb
+++ spec/unit/hook_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
 describe Premailer::Rails::Hook do
-  describe ".perform" do
-    let(:message) { double :message, header: header }
-    let(:perform_hook) { Premailer::Rails::Hook.perform(message) }
+  describe "#perform" do
+    let(:hook) { described_class.new message }
+
+    let(:message) { double :message, header: header, content_type: content_type }
+    let(:perform_hook) { hook.perform }
+    let(:rails_config) { { } }
+    let(:header) { { } }
+    let(:content_type) { "text/html, something/else"}
+
+    before(:each) do
+      allow(Premailer::Rails).to receive(:config).and_return rails_config
+    end
 
     class << self
       def it_doesnt_inline_styles
         it "doesn't inline styles" do
-          expect(described_class).not_to receive(:replace_html_part)
+          expect(hook).not_to receive(:replace_html_part)
           perform_hook
         end
       end
@@ -25,8 +34,32 @@ describe Premailer::Rails::Hook do
       it_doesnt_inline_styles
     end
 
-    context "when the skip_premailer is not set" do
-      let(:header) { }
+    context "and Rails.config[:disable_premailer] is set" do
+      let(:rails_config) { { disable_premailer: true } }
+
+      context "and the header has no enable_premailer" do
+        it_doesnt_inline_styles
+      end
+
+      context "and the header has enable_premailer" do
+        let(:header) { { enable_premailer: true } }
+
+        before(:each) do
+          allow(hook).to receive(:generate_html_part_replacement).and_return "replacement"
+          allow(hook).to receive(:replace_html_part)
+        end
+
+        it "removes enable_premailer header" do
+          perform_hook
+          expect(header[:enable_premailer]).to be_nil
+        end
+
+        it "inlines CSS" do
+          expect(hook).to receive(:replace_html_part).with("replacement")
+          perform_hook
+        end
+
+      end
     end
   end
 end
