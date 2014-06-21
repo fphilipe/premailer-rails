82ad13d04b0a080e850b7fecb818e66d25c9e958
Philipe Fatio <me@phili.pe>
2014-06-21 21:58:22 +0200

    Clean up after spec

--- spec/unit/premailer_rails_spec.rb
+++ spec/unit/premailer_rails_spec.rb
@@ -4,7 +4,15 @@ describe Premailer::Rails do
   describe '#config' do
     subject { Premailer::Rails.config }
     context 'when set' do
-      before { Premailer::Rails.config = { foo: :bar } }
+      around do |example|
+        begin
+          default_config = described_class.config
+          described_class.config = { foo: :bar }
+          example.run
+        ensure
+          described_class.config = default_config
+        end
+      end
       it { should == { foo: :bar } }
     end
   end
