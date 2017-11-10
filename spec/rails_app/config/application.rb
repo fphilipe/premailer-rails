ae4fb3928ff19662ad457f79695abca1dbd4a5f7
Philipe Fatio <me@phili.pe>
2017-11-10 08:17:40 +0100

    Remove clutter from dummy rails app

--- spec/rails_app/config/application.rb
+++ spec/rails_app/config/application.rb
@@ -1,9 +1,5 @@
 require_relative 'boot'
 
-# require "rails/all"
-# Pick the frameworks you want:
-# require "active_model/railtie"
-# require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
@@ -13,8 +9,5 @@ Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
--- spec/rails_app/config/environment.rb
+++ spec/rails_app/config/environment.rb
@@ -1,5 +1,2 @@
-# Load the Rails application.
 require_relative 'application'
-
-# Initialize the Rails application.
 Rails.application.initialize!
