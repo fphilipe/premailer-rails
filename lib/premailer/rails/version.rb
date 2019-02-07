8a53f9b1ea470838fbdf952f433bb8625a66943f
Arslan Javed <ar31an@gmail.com>
2019-02-08 00:23:16 +0500

    Add the frozen_string_literal pragma comment

--- lib/premailer/rails/version.rb
+++ lib/premailer/rails/version.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Premailer
   module Rails
     VERSION = File.read(
       File.expand_path('../../../../VERSION', __FILE__)
-    ).strip
+    ).strip.freeze
   end
 end
