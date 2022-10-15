4bc3f81f51e0e2fc53da4d75e01f7c7d06fe816d
Philipe Fatio <me@phili.pe>
2022-10-15 14:35:06 +0200

    Ditch Simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,3 @@
-if RUBY_ENGINE == 'ruby'
-  unless ENV['CI']
-    require 'simplecov'
-    SimpleCov.start
-  end
-end
-
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../spec/rails_app/config/environment.rb", __FILE__)
