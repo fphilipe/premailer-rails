6ae0861a3df600778d53eb26ef90826aa365ee44
Philipe Fatio <me@phili.pe>
2022-10-15 21:19:10 +0200

    Add example app for Rails 6

---
+++ examples/rails6/app/assets/config/manifest.js
@@ -0,0 +1,1 @@
+//= link email.css
---
+++ examples/rails6/config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Example
+  class Application < Rails::Application
+    config.load_defaults 6.1
+  end
+end
