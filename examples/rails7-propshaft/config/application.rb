eb650cae300acb10a5ddba4c9a917697ff72457f
Philipe Fatio <me@phili.pe>
2022-10-15 22:20:25 +0200

    Add example app for Rails 7 with Propshaft

---
+++ examples/rails7-propshaft/config/application.rb
@@ -0,0 +1,14 @@
+require_relative 'boot'
+
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Example
+  class Application < Rails::Application
+    config.load_defaults 7.0
+  end
+end
